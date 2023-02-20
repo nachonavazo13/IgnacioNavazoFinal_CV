54909606282ccdd06db734b001083d5711f49ce6
ignacio <nachonavazo13@hotmail.com>
2023-02-20 12:25:36 -0300

    Ampliamos animaciones en el doc. app.js

--- app.js
+++ app.js
@@ -24,3 +24,45 @@ for(var x = 0; x < links.length; x++){
     }
 }
 
+// Animaciones Barras de % identificadas por si id
+
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+
+    }
+}
+
+// Seleccionar todas las barras generales para luego manipularlas
+
+let html = document.getElementById("html");
+crearBarra(html);
+
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+
+let wordpress = document.getElementById("wordpress");
+crearBarra(wordpress);
+
+let photoshop = document.getElementById("photoshop");
+crearBarra(photoshop);
+
+let php = document.getElementById("php");
+crearBarra(php);
+
+let ilustrator = document.getElementById("ilustrator");
+crearBarra(ilustrator);
+
+// Se eliminaron los elementos "e" del index.html 1hs 03'
+
+// Marco el porcentaje en las barras (Pintamos la barra segun el %).
+
+// Inicia en -1 por que no tiene ninguna celda pintada.
+
+let contadores = [-1,-1,-1,-1,-1,-1]
+
+
+
+
